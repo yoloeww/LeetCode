7587f678643de1164c60aab85d018312c754e992
yolo <77111763+yoloeww@users.noreply.github.com>
2022-11-23 14:58:28 +0800

    Create 137.只出现一次的数字2.cpp

---
+++ 137.只出现一次的数字2.cpp
@@ -0,0 +1,22 @@
+class Solution
+ {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        unordered_map<int, int> freq;
+        for (int num: nums) 
+        {
+            ++freq[num];
+        }
+        int end = 0;
+        for (auto [num, occ]: freq) 
+        {
+            if (occ == 1) 
+            {
+                end = num;
+                break;
+            }
+        }
+        return end;
+    }
+ };

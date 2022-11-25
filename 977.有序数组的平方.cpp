75638b19bd6f29ec7752d7bbb19161ab64dd410e
yolo <77111763+yoloeww@users.noreply.github.com>
2022-11-25 19:22:31 +0800

    Create 977.有序数组的平方.cpp

---
+++ 977.有序数组的平方.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+       int i = 0;
+       for(i = 0; i < nums.size(); i++)
+       {
+           nums[i] = nums[i] * nums[i];
+       }
+       sort(nums.begin(),nums.end());
+       return nums;
+    }
+};

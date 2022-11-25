5c4dac131006cf8e9374cf0547bfccc166f81ff9
yolo <77111763+yoloeww@users.noreply.github.com>
2022-11-25 15:46:12 +0800

    Create 35. 搜索插入位置.cpp

---
+++ 35. 搜索插入位置.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) 
+    {
+        int left = 0, right = nums.size() - 1;
+        int middle;
+        while(left <= right)
+        {
+            middle = (left + right)/2;
+            if(target > nums[middle])
+            {
+                left = middle + 1;
+            }
+            else 
+            {
+                right = middle - 1;
+            }
+        }
+        return left;
+    }
+};

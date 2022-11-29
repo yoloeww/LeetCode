277538b8675c57fd4bb2f096179b008836531a12
yolo <77111763+yoloeww@users.noreply.github.com>
2022-11-29 10:05:13 +0800

    Create 203.移除list元素.cpp

---
+++ 203.移除list元素.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) 
+    {
+           ListNode* dummyHead = new ListNode(0);
+           dummyHead -> next = head;
+           ListNode* cur = dummyHead;
+           while(cur -> next != nullptr)
+           {
+               if(cur -> next -> val == val)
+               { 
+                   ListNode* tmp = cur -> next;
+                   cur -> next = cur -> next -> next;
+                   delete tmp;
+               }
+               else
+               {
+                   cur = cur -> next;
+               }
+           }
+           head = dummyHead -> next;
+           delete dummyHead;
+           return head;
+    }
+};

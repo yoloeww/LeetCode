3ed03fba059e9d4135a0bf1bcc905d3a6ca1be9a
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-18 16:18:56 +0800

    Create 20.有效的括号.cpp

---
+++ 20.有效的括号.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      if (s.size() % 2 != 0) return false; // 如果s的长度为奇数，一定不符合要求
+        stack <char> use;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == '(') 
+            use.push(')');
+            else if (s[i] == '{')
+            use.push('}');
+            else if (s[i] == '[') 
+            use.push(']');
+            else if (use.empty() || use.top() != s[i])
+            return false;
+            else
+            use.pop();
+        }
+        return use.empty();
+    }
+};

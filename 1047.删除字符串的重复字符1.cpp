a6740299b2f9d3dd6b3b982ceaa480d0861fd1d1
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-18 16:30:31 +0800

    Create 1047.删除字符串的重复字符1.cpp

---
+++ 1047.删除字符串的重复字符1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+    stack <char> st;
+    for(char tmp:s) {
+        if(st.empty() || tmp != st.top())
+        st.push(tmp);
+        else {
+            st.pop();
+        }
+    }
+    string result;
+    while(!st.empty()) { // 将栈中元素放到result字符串汇总
+            result += st.top();
+            st.pop();
+    }
+     reverse (result.begin(), result.end()); // 此时字符串需要反转一下
+        return result;
+    }
+};

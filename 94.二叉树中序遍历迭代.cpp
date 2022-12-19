de8aff390f1557c1863b1aa62a14063025596ce1
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-19 15:23:29 +0800

    Create 94.二叉树中序遍历迭代.cpp

---
+++ 94.二叉树中序遍历迭代.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ //递归法
+class Solution {
+public:
+    void traversal (TreeNode* cur,vector <int> &vec) {
+        if(cur == nullptr)
+        return; 
+        traversal(cur -> left,vec);
+        vec.push_back(cur -> val);
+        traversal(cur -> right,vec);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+    vector <int> result;
+    traversal(root, result);
+    return result;
+    }
+};

c9d6023c3d73f54c105e00f2e26a8a7869d63481
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-20 21:25:11 +0800

    Create 最大深度.cpp

---
+++ 最大深度.cpp
@@ -0,0 +1,21 @@
+class solution {
+public:
+    int result;
+    void getdepth(treenode* node, int depth) {
+        result = depth > result ? depth : result; // 中
+        if (node->left == NULL && node->right == NULL) return ;
+        if (node->left) { // 左
+            getdepth(node->left, depth + 1);
+        }
+        if (node->right) { // 右
+            getdepth(node->right, depth + 1);
+        }
+        return ;
+    }
+    int maxdepth(treenode* root) {
+        result = 0;
+        if (root == 0) return result;
+        getdepth(root, 1);
+        return result;
+    }
+};

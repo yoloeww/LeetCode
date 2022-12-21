2c4fb06083f3182e44ca956ea6aca9b966f450d6
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-21 21:22:06 +0800

    Create 111.cpp

---
+++ 111.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int getDepth(TreeNode* node) {
+        if (node == nullptr) return 0;
+        int leftDepth = getDepth(node -> left);
+        int rightDepth = getDepth(node -> right);
+        if(node -> left == nullptr && node -> right != nullptr)
+        return 1 + rightDepth;
+        if (node -> left != NULL && node -> right == NULL) 
+        return 1 + leftDepth;
+        int result = 1 + min(leftDepth, rightDepth);
+        return result;
+    }
+    int minDepth(TreeNode* root) {
+        return getDepth(root);
+    }
+};

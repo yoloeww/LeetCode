39683bab732290fed933043771dedb39665e93cc
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-23 22:05:56 +0800

    Create 513.cpp

---
+++ 513.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> que;
+        if (root != NULL) que.push(root);
+        int result = 0;
+        while (!que.empty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = que.front();
+                que.pop();
+                if (i == 0) result = node->val; // 记录最后一行第一个元素
+                if (node->left) que.push(node->left);
+                if (node->right) que.push(node->right);
+            }
+        }
+        return result;
+    }
+};

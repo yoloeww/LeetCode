96f54a5d2874331051abc9b8c13b8a9caf6d9fbe
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-24 21:16:16 +0800

    Create 112.路径总和.cpp

---
+++ 112.路径总和.cpp
@@ -0,0 +1,10 @@
+class solution {
+public:
+    bool hasPathSum(TreeNode* root, int sum) {
+        if (root == null) return false;
+        if (!root->left && !root->right && sum == root->val) {
+            return true;
+        }
+        return haspathsum(root->left, sum - root->val) || haspathsum(root->right, sum - root->val);
+    }
+};

221a1aedeedea1ee3c76f99d0d73ae7abc5d8097
yolo <77111763+yoloeww@users.noreply.github.com>
2022-12-30 19:56:40 +0800

    Create 455.分发饼干.cpp

---
+++ 455.分发饼干.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+    int child = 0,cookie = 0;
+    int max = 0;
+    sort(g.begin(),g.end());
+    sort(s.begin(),s.end());
+    while (child < g.size() && cookie < s.size()) {
+        if (g[child] <= s[cookie]) {
+            child ++;
+        }
+        cookie++;
+    }
+    return child;
+    }
+};

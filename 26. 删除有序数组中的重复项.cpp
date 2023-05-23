cd8a8d0d8d9190e0d2ef189add3334ab2b9a997d
yolo <77111763+yoloeww@users.noreply.github.com>
2023-05-23 21:32:23 +0800

    Create 26. 删除有序数组中的重复项.cpp

---
+++ 26. 删除有序数组中的重复项.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        int slow = 0;
+        for (int fast = 0;  fast < nums.size();fast++) {
+            if (nums[fast] != nums[slow]) {
+                nums[++slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
+};

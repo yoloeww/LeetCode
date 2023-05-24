11e4f0f66b601aaa80e87751a1f562dc642be698
yolo <77111763+yoloeww@users.noreply.github.com>
2023-05-24 21:39:14 +0800

    Create 19. 删除链表的倒数第 N 个结点.cpp

---
+++ 19. 删除链表的倒数第 N 个结点.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy -> next= head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        n++;
+        if (head == nullptr)
+            return nullptr;
+        ListNode* cur = head;
+        while(n -- && fast != nullptr) {
+        fast = fast -> next;
+        }
+        while (fast) {
+            fast = fast -> next;
+            slow = slow -> next;
+        }
+        ListNode* temp = slow -> next;
+        slow -> next = slow -> next -> next;
+        delete temp;
+        return dummy -> next;
+    }
+};

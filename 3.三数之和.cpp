0a9a95182731ba31be6fddabd91728538aa8bdf2
yolo <77111763+yoloeww@users.noreply.github.com>
2023-05-24 21:40:24 +0800

    Create 3.三数之和.cpp

---
+++ 3.三数之和.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> result;
+        for (int i = 0; i < nums.size(); i ++) {
+            if (nums[i] > 0)
+                return result;
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (left < right) {
+                if (nums[i] + nums[left] + nums[right] > 0) right--;
+                else if (nums[i] + nums[left] + nums[right] < 0) left++;
+                else {
+                    result.push_back(vector<int>{nums[i], nums[left], nums[right]});
+                    while (right > left && nums[right] == nums[right - 1]) right --;
+                    while (right > left && nums[left]== nums[left + 1]) left++;
+
+                    right --;
+                    left ++;
+                }
+            }
+        }
+        return result;
+    }
+};

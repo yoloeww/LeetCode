05d0a772b490a09fc97acce3550b7b6976af9686
yolo <77111763+yoloeww@users.noreply.github.com>
2023-06-06 21:55:29 +0800

    Create 11. 盛最多水的容器.cpp

---
+++ 11. 盛最多水的容器.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0,j = height.size() - 1,res = 0;
+
+        // 选定两板高度中的短板，向中间收窄一格；
+        while (i < j) {
+            res = height[i] < height[j] ?
+            max(res,(j - i) * height[i ++]) :
+            max(res,(j - i) * height[j --]);
+        }
+        return res;
+    }
+};

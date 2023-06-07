545fb110cf1c4e7c979fa14b5c88b60af0809890
yolo <77111763+yoloeww@users.noreply.github.com>
2023-06-07 21:58:52 +0800

    Create 114. 二叉树展开为链表.cpp

---
+++ 114. 二叉树展开为链表.cpp
@@ -0,0 +1,33 @@
+114. 二叉树展开为链表
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        vector<TreeNode*> res;
+        pre(root,res);
+        int n = res.size();
+        for (int i = 1; i < n; i++) {
+             TreeNode *prev = res.at(i - 1),*cur = res.at(i);
+             prev -> left = nullptr;
+             prev -> right = cur;
+        }
+    }
+    void pre(TreeNode* root,vector<TreeNode* >& res) {
+        if (root) {
+            res.push_back(root);
+            pre(root -> left,res);
+            pre(root -> right,res);
+        }
+    }
+};

315a87c992485bd10de887db0423dee202ae4688
yolo <77111763+yoloeww@users.noreply.github.com>
2023-06-08 21:52:08 +0800

    Create 1047. 移除字符串的相邻字符2.CPP

---
+++ 1047. 移除字符串的相邻字符2.CPP
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string S) {
+        stack<char> st;
+        for (char s : S) {
+            if (st.empty() || s != st.top()) {
+                st.push(s);
+            } else {
+                st.pop(); // s 与 st.top()相等的情况
+            }
+        }
+        string result = "";
+        while (!st.empty()) { // 将栈中元素放到result字符串汇总
+            result += st.top();
+            st.pop();
+        }
+        reverse (result.begin(), result.end()); // 此时字符串需要反转一下
+        return result;
+
+    }
+};

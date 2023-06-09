3e4d44afab8a9f9fd53d83f52b61498d45f0288a
yolo <77111763+yoloeww@users.noreply.github.com>
2023-06-09 21:13:47 +0800

    Create 209.最小长度子数组.CPP

---
+++ 209.最小长度子数组.CPP
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+        int curSum = 0;
+        int minLength = 0;
+
+        while (right < nums.size()) {
+            curSum += nums[right];
+            while (curSum >= target) {
+                if (right - left + 1 < minLength || minLength == 0) {
+                    minLength = right - left + 1;
+                }
+                curSum = curSum - nums[left];
+                left ++;
+            }
+            right ++;
+        }
+        return minLength;
+    }
+};

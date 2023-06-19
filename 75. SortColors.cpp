d8b8f4697bc06101d27aa4fc7757e28bc09783fc
yolo <77111763+yoloeww@users.noreply.github.com>
2023-06-19 21:30:34 +0800

    Create 75. SortColors.cpp

---
+++ 75. SortColors.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        int p0 = 0,p2 = n - 1;
+        for (int i = 0; i <= p2; i ++) {
+            while (i <= p2 && nums[i] == 2) {
+                swap(nums[i],nums[p2]);
+                --p2;
+            }
+            if (nums[i] == 0) {
+                swap(nums[i],nums[p0]);
+                ++p0;
+            }
+        }
+    }
+};

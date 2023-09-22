bf2934216e0f0b0edacfce754e39f5c196f451a6
yolo <77111763+yoloeww@users.noreply.github.com>
2023-09-22 22:13:55 +0800

    Create 51.N皇后.cpp

---
+++ 51.N皇后.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+vector<vector<string>> res;
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board (n,string(n,'.'));
+        backtrack(board,0);
+        return res;
+    }
+    void backtrack(vector<string>& board, int row) {
+        if (row == board.size()) {
+            res.push_back(board);
+            return;
+        }
+        int n = board[row].size();
+        for (int col = 0; col < n; col++) {
+            if (!isvaild(board,row,col)) {
+                continue;
+            }
+            board[row][col] = 'Q';
+             backtrack(board, row + 1);
+            // 撤销选择
+            board[row][col] = '.';
+        }
+    }
+    bool isvaild(vector<string>& board, int row, int col) {
+        int n = board.size();
+        for (int i = 0; i <= row; i++) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+        for (int i = row - 1,j = col + 1; i >= 0 && j < n;i --,j ++) { // 左上
+            if (board[i][j] == 'Q')
+            return false;
+        }
+        for (int i = row - 1, j = col - 1;i >= 0 && j >= 0; i--, j--) {  // 右上
+            if (board[i][j] == 'Q')
+            return false;
+        }
+        return true;
+    }
+};

e00f6ee58a330c0b2ca52ce6bee01892f1c42259
yolo <77111763+yoloeww@users.noreply.github.com>
2023-10-12 22:09:54 +0800

    Create 1. 两数之和.cpp

---
+++ 1. 两数之和.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        if (nums.size() == 0) return {0,0};
+        unordered_map<int,int> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            auto it = mp.find(target - nums[i]);
+            if (it != mp.end()) {
+                return {it -> second,i};
+            }
+            mp[nums[i]] = i;
+        }
+        return {-1,-1};
+
+    }
+};

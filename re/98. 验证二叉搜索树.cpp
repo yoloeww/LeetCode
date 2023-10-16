65d7ce28b9401ec0fd3c33287524a419db47ecdd
yolo <77111763+yoloeww@users.noreply.github.com>
2023-10-16 22:08:41 +0800

    Create 98. 验证二叉搜索树.cpp

---
+++ re/98. 验证二叉搜索树.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+long long maxVal = LONG_MIN;
+    bool isValidBST(TreeNode* root) {
+        if (root == nullptr) return true;
+        bool left = isValidBST(root -> left);
+        if (maxVal < root -> val) maxVal = root -> val;
+        else return false;
+        bool right = isValidBST(root -> right);
+        return left && right;
+    }
+};

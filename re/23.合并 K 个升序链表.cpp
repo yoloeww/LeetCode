aca70dfa6394849eb80bf4f058c6103d109c318e
yolo <77111763+yoloeww@users.noreply.github.com>
2023-10-19 21:57:57 +0800

    Create 23.合并 K 个升序链表.cpp

---
+++ re/23.合并 K 个升序链表.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode *a, ListNode *b) {
+        if ((!a) || (!b)) return a ? a : b;
+        ListNode head, *tail = &head, *aPtr = a, *bPtr = b;
+        while (aPtr && bPtr) {
+            if (aPtr -> val < bPtr -> val) {
+                tail -> next = aPtr;
+                aPtr = aPtr -> next;
+            }
+            else {
+                tail->next = bPtr;
+                bPtr = bPtr->next;
+            }
+            tail = tail -> next;
+        }
+        tail -> next = (aPtr ? aPtr : bPtr);
+        return head.next;
+    }
+    ListNode* merge(vector<ListNode*> &lists,int l,int r) {
+        if (l == r) return lists[l];
+        if (l > r) return nullptr;
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        return merge(lists, 0, lists.size() - 1);
+    }
+};

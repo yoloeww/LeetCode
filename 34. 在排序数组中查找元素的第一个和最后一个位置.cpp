5365589835a25cbe8bfac7d8e98706651a7a18c0
yolo <77111763+yoloeww@users.noreply.github.com>
2023-10-21 22:05:05 +0800

    Create 34. 在排序数组中查找元素的第一个和最后一个位置.cpp

---
+++ 34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -0,0 +1,55 @@
+class Solution 
+{
+public:
+    vector<int> searchRange(vector<int>& nums, int target) 
+    {
+        int leftBorder = getLeftBorder(nums, target);
+        int rightBorder = getRightBorder(nums, target);
+        if(leftBorder == -2 || rightBorder == -2)
+        return {-1,-1};
+        if (rightBorder - leftBorder > 1) 
+        return {leftBorder + 1, rightBorder - 1};
+        return {-1, -1};
+    }
+private:
+     int getRightBorder(vector<int>& nums, int target) 
+     {
+        int left = 0;
+        int right = nums.size() - 1;
+        int rightBorder = -2; // 记录一下rightBorder没有被赋值的情况
+        while (left <= right) 
+        {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] > target) 
+            {
+                right = middle - 1;
+            } 
+            else 
+            { // 寻找右边界，nums[middle] == target的时候更新left
+                left = middle + 1;
+                rightBorder = left;
+            }
+        }
+        return rightBorder;
+    }
+     int getLeftBorder(vector<int>& nums, int target) 
+     {
+        int left = 0;
+        int right = nums.size() - 1;
+        int leftBorder = -2; // 记录一下rightBorder没有被赋值的情况
+        while (left <= right) 
+        {
+            int middle = left + ((right - left) / 2);
+            if (nums[middle] >= target) 
+            {
+                right = middle - 1;
+                leftBorder = right;
+            } 
+            else 
+            { // 寻找右边界，nums[middle] == target的时候更新left
+                left = middle + 1;
+            }
+        }
+        return leftBorder;
+    }
+};

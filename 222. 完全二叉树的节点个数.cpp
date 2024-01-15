d7b0d249377870fbc5ab7b5dc85d82d9ef0bcf1b
yolo <77111763+yoloeww@users.noreply.github.com>
2024-01-15 22:39:05 +0800

    Update 222. 完全二叉树的节点个数.cpp

--- 222. 完全二叉树的节点个数.cpp
+++ 222. 完全二叉树的节点个数.cpp
@@ -9,7 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+class Solution 
 public:
     bool compare(TreeNode* left,TreeNode* right) {
         if (left == nullptr && right != nullptr) return false;

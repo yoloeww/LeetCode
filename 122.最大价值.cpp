e1969938585f967002a2075b104d49a28258bea1
yolo <77111763+yoloeww@users.noreply.github.com>
2024-02-08 21:32:27 +0800

    Update 122.最大价值.cpp

--- 122.最大价值.cpp
+++ 122.最大价值.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& price) {
     int result = 0;
     for (int i = 1; i < prices.size(); i++) {
         result += max(prices[i] - prices[i - 1], 0);

0d8797f0a63c9c53c7591f0a048384d55a053861
yolo <77111763+yoloeww@users.noreply.github.com>
2024-03-06 21:31:39 +0800

    Create 234. 回文链表.cpp

---
+++ 234. 回文链表.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        if (!head || !head->next) {
+            return true;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        slow = reverse(slow);
+
+        ListNode* p1 = head;
+        ListNode* p2 = slow;
+        while (p1 && p2) {
+            if (p1->val != p2->val) {
+                return false;
+            }
+            p1 = p1->next;
+            p2 = p2->next;
+        }
+        return true;
+    }
+};

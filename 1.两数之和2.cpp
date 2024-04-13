7e572fed99b27329e77c206b3fd6bba7ab98bade
yolo <77111763+yoloeww@users.noreply.github.com>
2024-04-13 23:17:54 +0800

    Update 1.两数之和2.cpp

--- 1.两数之和2.cpp
+++ 1.两数之和2.cpp
@@ -1,6 +1,6 @@
 using namespace std；
 class Solution 
-public：
+public： 
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map <int, int> map;
         for(int i = 0;i < nums.size(); i++) {

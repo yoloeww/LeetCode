5d5b622a19cd76d2c980134d96eb9926dc7fc190
yolo <77111763+yoloeww@users.noreply.github.com>
2024-06-18 23:10:30 +0800

    Update 128.最长连续序列.cpp

--- 128.最长连续序列.cpp
+++ 128.最长连续序列.cpp
@@ -1,6 +1,6 @@
 // 对于匹配的过程，暴力的方法是 O(n) 遍历数组去看是否存在这个数，但其实更高效的方法是用一个哈希表存储数组中的数，这样查看一个数是否存在即能优化至 O(1) 的时间复杂度
 // 那么怎么判断是否跳过呢？由于我们要枚举的数 x 一定是在数组中不存在前驱数x−1的，不然按照上面的分析我们会从 x−1 开始尝试匹配，因此我们每次在哈希表中检查是否存在 x−1即能判断是否需要跳过了。
-class Solution { 
+class Solution {  
 public:
     int longestConsecutive(vector<int>& nums) {
         unordered_set<int> mp;

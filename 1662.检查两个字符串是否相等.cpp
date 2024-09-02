4ec8a8a1288a7c81101f8ba4a840f02fe0a6857e
yolo <77111763+yoloeww@users.noreply.github.com>
2024-09-02 22:42:42 +0800

    Update 1662.检查两个字符串是否相等.cpp

--- 1662.检查两个字符串是否相等.cpp
+++ 1662.检查两个字符串是否相等.cpp
@@ -2,7 +2,6 @@ class Solution {
 public: 
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) 
     {
-        
         string newstr1,newstr2;
         for(int i = 0;i <word1.size();i++)
         {

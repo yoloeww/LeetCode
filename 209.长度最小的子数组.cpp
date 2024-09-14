2f4d62e8a1e5a0b7dc85e775be69bf0c26337868
yolo <77111763+yoloeww@users.noreply.github.com>
2024-09-14 21:29:38 +0800

    Update 209.长度最小的子数组.cpp

--- 209.长度最小的子数组.cpp
+++ 209.长度最小的子数组.cpp
@@ -7,7 +7,7 @@ public:
         int subLength = 0; // 滑动窗口的长度
         for (int j = 0; j < nums.size(); j++) {
             sum += nums[j];
-            // 注意这里使用while，每次更新 i（起始位置），并不断比较子序列是否符合条件
+            // // 注意这里使用while，每次更新 i（起始位置），并不断比较子序列是否符合条件
             while (sum >= s) {
                 subLength = (j - i + 1); // 取子序列的长度
                 result = result < subLength ? result : subLength;

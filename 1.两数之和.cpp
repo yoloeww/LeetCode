dc4d0198c0086500497e1494824694af8c088729
yolo <77111763+yoloeww@users.noreply.github.com>
2024-09-24 22:08:11 +0800

    Update 1.两数之和.cpp

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:   
    vector<int>twoSum(vector<int>& nums, int target) 
-        int i = 0,j = 1; 
+        int i = 0,j = 1;
         int maxArea = nums.size()-1;
         while (nums[i] + nums[j] != target)
               if(j == maxArea)

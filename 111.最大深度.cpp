bb805989a146b9d66445299917fecee6313938d0
yolo <77111763+yoloeww@users.noreply.github.com>
2024-10-09 21:49:45 +0800

    Update 111.最大深度.cpp

--- 111.最大深度.cpp
+++ 111.最大深度.cpp
@@ -15,7 +15,7 @@ public:
         if (node == nullptr) return 0;
         int leftDepth = getDepth(node -> left);
         int rightDepth = getDepth(node -> right);
-        if(node -> left == nullptr && node -> right != nullptr)
+        if(node -> left == nullptr && node -> right  = nullptr)
         return 1 + rightDepth;
         if (node -> left != NULL && node -> right == NULL) 
         return 1 + leftDepth;

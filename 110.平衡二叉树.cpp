6be7a851a8d60143dcef1ee23c506f1edfce0793
yolo <77111763+yoloeww@users.noreply.github.com>
2025-04-25 23:48:05 +0800

    Update 110.平衡二叉树.cpp

--- 110.平衡二叉树.cpp
+++ 110.平衡二叉树.cpp
@@ -9,7 +9,6 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
-
 class Solution {
 public:
     int getHeight(TreeNode* node) {

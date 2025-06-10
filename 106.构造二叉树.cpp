dda572c40c8ace845d117c84691887753da86aeb
yolo <77111763+yoloeww@users.noreply.github.com>
2025-06-10 23:37:09 +0800

    Update 106.构造二叉树.cpp

--- 106.构造二叉树.cpp
+++ 106.构造二叉树.cpp
@@ -1,5 +1,6 @@
 class Solution {
 private:   
+
     TreeNode* traversal (vector<int>& inorder, vector<int>& postorder) {
         if (postorder.size() == 0) return NULL
         // 后序遍历数组最后一个元素，就是当前的中间节点
